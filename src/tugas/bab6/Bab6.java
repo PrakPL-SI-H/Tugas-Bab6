9c568ab26a2fbb2db671d986c9521aab2582d9b6
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-04 01:20:04 +0700

    edit main class

--- src/tugas/bab6/Bab6.java
+++ src/tugas/bab6/Bab6.java
@@ -14,10 +14,21 @@ public class Bab6 {
     public static void main(String[] args) {
         Employee manager = new Employee("Budi",2121,"manajer","tidak",0,2014);
         Employee Pekerjatetap = new Employee("Santoso",2441,"OB","iya",3,2010);
-        Employee Pekerjatetap1 = new Employee("Aji", 2442,"Security","iya",5,2011);
+        Employee Pekerjatetap1 = new Employee("Aji", 2442,"Security","iya",4,2011);
         Employee Pekerjatetap2 = new Employee("Anto", 2443,"Teknisi","iya",2,2012);
         Employee Pekerjatetap3 = new Employee("Salim", 2444,"Sopir","iya",1,2013);
         Employee Pekerjatetap4 = new Employee("Santi",2445,"Sekertaris","iya",2,2012);
+        Employee Pekerjatetap5 = new Employee("Santoso",2446,"OB","iya",3,2010);
+        Employee Pekerjatetap6 = new Employee("Ayu", 2447,"OB","iya",5,2011);
+        Employee Pekerjatetap7 = new Employee("Jaya", 2448,"Teknisi","iya",0,2012);
+        Employee Pekerjatetap8 = new Employee("Sandy", 2449,"Sopir","iya",1,2013);
+        Employee Pekerjatetap9 = new Employee("Dewi",2450,"Sekertaris","iya",2,2012);
+        Employee Pekerjatetap10 = new Employee("Sri",2451,"OB","iya",3,2010);
+        Employee Pekerjatidaktetap = new Employee("Ahmad", 2452,"Security","iya",5,2011);
+        Employee Pekerjatidaktetap2 = new Employee("Deny", 2453,"Teknisi","iya",2,2012);
+        Employee Pekerjatidaktetap3 = new Employee("Sari", 2454,"Sekertaris","iya",4,2013);
+        Employee Pekerjatidaktetap4 = new Employee("Sinta",2455,"Sekertaris","iya",2,2014);
+        Employee Pekerjatidaktetap5 = new Employee("Rendy",2456,"Teknisi","iya",2,2015);
     }
     
 }
