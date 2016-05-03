41e259cb81459833ff8bb1732f0b4132179ad513
FajarAH <akbaransyah@gmail.com>
2016-05-04 04:19:27 +0700

    membuat method get()

--- LaporanBab6/src/Employee.java
+++ LaporanBab6/src/Employee.java
@@ -5,6 +5,7 @@ public class Employee {
     private String jabatan;
     private boolean istri;
     private boolean anak;
+    private int hireYear;
     private double bonus;
     private double salary;
     private int lamaKerja;
@@ -13,5 +14,36 @@ public class Employee {
     private double tunjanganIstri ;
     private double tunjanganAnak;
     private int jumlahAnak;
+    
+      public Employee(String name, String idKerja, String jabatan, boolean istri, boolean anak, int hireYear, int jumnak) {
+        this.name = name;
+        this.idKerja = idKerja;
+        this.jabatan = jabatan;
+        this.istri = istri;
+        this.anak = anak;
+        this.hireYear = hireYear;
+        this.jumlahAnak = jumnak;
+    }
+      
+       public int getLamaKerja() {
+        lamaKerja = 2016 - hireYear;
+        return lamaKerja;
+    }
+
+    public String getName() {
+        return name;
+    }
+     public String getIDKerja() {
+        return idKerja;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public int getHireYear() {
+        return hireYear;
+    }
+      
     
 }
