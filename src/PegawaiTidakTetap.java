d772d3f6af18d0b9d8fbd5c502be1285111e325e
arifardhan <ariframadhan902@yahoo.com>
2016-05-03 21:30:48 +0700

    Membuat method setJamKerja pada Class PegawaiTidakTetap

--- src/PegawaiTidakTetap.java
+++ src/PegawaiTidakTetap.java
@@ -8,5 +8,9 @@ public class PegawaiTidakTetap extends Employee {
     public PegawaiTidakTetap(String n, String id, String j, String i, String a, int m, int x) {
         super(n, id, j, i, a, m, x);
     }
-    
+
+    public void setJamKerja(int j) {
+        jam = j;
+    }
+
 }
