dad82c1b1f83e449b64f0c70f77cc10dbc4f408f
arifardhan <ariframadhan902@yahoo.com>
2016-05-03 21:44:24 +0700

    Membuat extends konstruktor pada class pegawai tetap dari superclass (Employee)

--- src/PegawaiTetap.java
+++ src/PegawaiTetap.java
@@ -2,4 +2,9 @@ package Praktikum6;
 
 public class PegawaiTetap extends Employee {
 
+    public PegawaiTetap(String n, String id, String j, String i, String a, int m, int x) {
+        super(n, id, j, i, a, m, x);
+    }
+
+    
 }
