3380833086d7b8d8dfbdc949fa9f31d44e6abc8e
Nofry <nofryarman@gmail.com>
2016-05-03 15:32:10 -0700

    pembuatan class pgwai tdk tetap

--- src/PegawaitdkTetap.java
+++ src/PegawaitdkTetap.java
@@ -1,13 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+public class PegawaitdkTetap extends Employee {
+    public int lembur;
+    public int jam;
+    public int gaji;
 
-/**
- *
- * @author Nofry
- */
-public class PegawaitdkTetap {
+    public PegawaitdkTetap(String nama, String jabatan, String idKerja, String istri, int anak, int thn, int jam) {
+        super(nama, jabatan, idKerja, istri, anak, thn);
+        this.jam = jam;
+    }
     
+    public void HitungGaji(){
+        if (jam > 10){
+            lembur = jam - 10;
+            this.gaji = gajiPokok + (10000 * lembur);
+        }
+        else{
+            this.gaji = gajiPokok;
+        }
+    }
+    
+    public void Tampilkan(){
+        System.out.println("===============================");
+        System.out.println("Nama Pegawai : "+nama);
+        System.out.println("Jabatan      : "+jabatan);
+        System.out.println("ID           : "+idKerja);
+        System.out.println("Tahun Masuk  : "+thn);
+        System.out.println("Gaji         : Rp."+gaji+"/Bulan");
+        System.out.println("===============================");
+    }
 }
