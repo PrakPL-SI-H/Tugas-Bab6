0f190942592e95c7d34e494d726be20132e7ed26
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-04 00:55:34 +0700

    Edit main class

---
+++ src/tugas/bab6/Bab6.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Inheritance;
+
+/**
+ *
+ * @author Prasetyo
+ */
+public class Bab6 {
+
+    public static void main(String[] args) {
+        Employee manager = new Employee("Budi",2121,"manajer","tidak",0,2014);
+        Employee Pekerjatetap = new Employee("Santoso",2441,"OB","iya",3,2010);
+        Employee Pekerjatetap1 = new Employee("Aji", 2442,"Security","iya",5,2011);
+        Employee Pekerjatetap2 = new Employee("Anto", 2443,"Teknisi","iya",2,2012);
+        Employee Pekerjatetap3 = new Employee("Salim", 2444,"Sopir","iya",1,2013);
+        Employee Pekerjatetap4 = new Employee("Santi",2445,"Sekertaris","iya",2,2012);
+    }
+    
+}
