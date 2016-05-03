a3a266a589352965544a8ca32ac6d94716471e5a
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-04 01:20:47 +0700

    edit method class

--- src/tugas/bab6/PekerjaTidakTetap.java
+++ src/tugas/bab6/PekerjaTidakTetap.java
@@ -11,10 +11,27 @@ package Inheritance;
  */
 public class PekerjaTidakTetap extends Employee {
     private double bonus;
+    private double tunjangan;
+    private String name;
+    private int Idkerja;
+    private String Jabatan;
+    private String Status;
+    private int anak;
+    private int Tahun;
     public PekerjaTidakTetap(String name,int Idkerja,String Jabatan,String Status,int anak,int Tahun){
         super (name, Idkerja, Jabatan, Status, anak, Tahun);//harus ada paling atas
-        bonus = 0.1 ;
-        System.out.println(name + Idkerja + Jabatan + Status + anak + Tahun);
+    }
+    @Override
+    public String Getnama(){
+    return name;
+    }
+    @Override
+    public String GetJabatan(){
+    return Jabatan;
+    }
+    @Override
+    public int GetIdKerja(){
+    return Idkerja;
     }
     
 }
