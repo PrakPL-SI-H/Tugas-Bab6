fa8d9075627f6c7704d4c61c7e93f6eecb752c68
FajarAH <akbaransyah@gmail.com>
2016-05-04 04:25:37 +0700

    membuat constructor class Manager

--- LaporanBab6/src/Manager.java
+++ LaporanBab6/src/Manager.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author asus
- */
-public class Manager {
-    
+public class Manager extends Employee {
+       public Manager(String name, String idKerja, String jabatan, boolean istri, boolean anak, int hireYear,int jumlahAnak) {
+        super(name, idKerja, jabatan, istri, anak, hireYear,jumlahAnak);
+    }
 }
