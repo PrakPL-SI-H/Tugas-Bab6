27e6b3ec6fa0e1640fbac38c4917904e1cf8bdfa
arifardhan <ariframadhan902@yahoo.com>
2016-05-03 23:52:15 +0700

    Mencetak informasi jika pegawai tetap

--- src/KepegawaianMain.java
+++ src/KepegawaianMain.java
@@ -30,25 +30,40 @@ public class KepegawaianMain {
             int jam = in.nextInt();
             System.out.println("");
             obj1.setJamKerja(jam);
+            System.out.println("");
             System.out.println("=====Informasi Pegawai=====");
-            System.out.println("nama    : " + nama);
-            System.out.println("id      : " + id);
-            System.out.println("jabatan : " + jabatan);
-            System.out.println("gaji        : Rp." + obj1.getGaji());
-            System.out.println("gaji lembur : Rp." + obj1.setLembur());
-            System.out.println("total       : Rp." + obj1.total());
-        } else if (jabatan.equals("manager")) {
+            System.out.println("Nama    : " + nama);
+            System.out.println("Id      : " + id);
+            System.out.println("Jabatan : " + jabatan);
+            System.out.println("Gaji pokok  : Rp." + obj1.getGaji());
+            System.out.println("Gaji lembur : Rp." + obj1.setLembur());
+            System.out.println("Total       : Rp." + obj1.total());
+        } else if (jabatan.equalsIgnoreCase("manager")) {
             Manager obj2 = new Manager(nama, id, jabatan, istri, anak, tahunmasuk, jum_anak);
             obj2.setJabatan(tahunmasuk);
+            System.out.println("");
             System.out.println("=====Informasi Pegawai=====");
-            System.out.println("nama    : " + nama);
-            System.out.println("id      : " + id);
-            System.out.println("jabatan : " + jabatan);
-            System.out.println("gaji        : Rp." + obj2.getGaji());
-            System.out.println("bonus       : Rp." + obj2.getBonus());
-            System.out.println("tunjangan   : Rp." + obj2.getTunjangan());
-            System.out.println("tun.jabatan : Rp." + obj2.getTunjanganJabatan());
-            System.out.println("total       : Rp." + obj2.total());
+            System.out.println("Nama    : " + nama);
+            System.out.println("Id      : " + id);
+            System.out.println("Jabatan : " + jabatan);
+            System.out.println("Gaji pokok  : Rp." + obj2.getGaji());
+            System.out.println("Bonus       : Rp." + obj2.getBonus());
+            System.out.println("Tunjangan   : Rp." + obj2.getTunjangan());
+            System.out.println("Tun.jabatan : Rp." + obj2.getTunjanganJabatan());
+            System.out.println("Total       : Rp." + obj2.total());
+        } else if (jabatan.equalsIgnoreCase("tetap")) {
+            PegawaiTetap obj3 = new PegawaiTetap(nama, id, jabatan, istri, anak, tahunmasuk, jum_anak);
+            obj3.setJabatan(tahunmasuk);
+            System.out.println("");
+            System.out.println("=====Informasi Pegawai=====");
+            System.out.println("Nama    : " + nama);
+            System.out.println("Id      : " + id);
+            System.out.println("Jabatan : " + jabatan);
+            System.out.println("Gaji pokok  : Rp." + obj3.getGaji());
+            System.out.println("Bonus       : Rp." + obj3.getBonus());
+            System.out.println("Tunjangan   : Rp." + obj3.getTunjangan());
+            System.out.println("Total       : Rp." + obj3.total());
+
         }
     }
 }
