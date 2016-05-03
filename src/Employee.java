100fa6a27f3e230c9de33e13e66d6f8f05cf0f09
Nofry <nofryarman@gmail.com>
2016-05-03 15:06:41 -0700

    edit class employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Nofry
- */
 public class Employee {
     public String nama;
     public String jabatan;
@@ -23,6 +13,5 @@ public class Employee {
         this.istri = istri;
         this.anak = anak;
         this.thn = thn;
-    }
-    
+    }   
 }
