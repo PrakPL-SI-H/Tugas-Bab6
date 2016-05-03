54c2aa0b59df0fd2923341b3e013614c7f3cdb27
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-04 01:20:34 +0700

    edit method class

--- src/tugas/bab6/Manager.java
+++ src/tugas/bab6/Manager.java
@@ -11,9 +11,30 @@ package Inheritance;
  */
 public class Manager extends Employee {
     private double bonus;
+    private double tunjangan;
+    private String name;
+    private int Idkerja;
+    private String Jabatan;
+    private String Status;
+    private int anak;
+    private int Tahun;
     public Manager(String name,int Idkerja,String Jabatan,String Status,int anak,int Tahun){
         super (name, Idkerja, Jabatan, Status, anak, Tahun);//harus ada paling atas
-        bonus = 0.1 ;
+    }
+    public void gaji(){
+        
+    }
+    @Override
+    public String Getnama(){
+    return name;
+    }
+    @Override
+    public String GetJabatan(){
+    return Jabatan;
+    }
+    @Override
+    public int GetIdKerja(){
+    return Idkerja;
     }
     
 }
