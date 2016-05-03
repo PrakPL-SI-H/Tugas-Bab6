9bbd2e9146456d85dec2702584a05606d98ac031
Nofry <nofryarman@gmail.com>
2016-05-03 15:21:09 -0700

    membuat class pegawai tetap

--- src/PegawaiTetap.java
+++ src/PegawaiTetap.java
@@ -1,13 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+public class PegawaiTetap extends Employee{
+    public int gaji;
+    public int lamaKerja;
 
-/**
- *
- * @author Nofry
- */
-public class PegawaiTetap {
+    public PegawaiTetap(String nama, String jabatan, String idKerja, String istri, int anak, int thn) {
+        super(nama, jabatan, idKerja, istri, anak, thn);
+    }
     
+    public void HitungGaji(){
+        lamaKerja = 2016 - thn;
+        if (lamaKerja <= 5){
+            this.gaji = gajiPokok;
+        }
+        else if (lamaKerja >= 6 && lamaKerja <=10){
+            this.gaji = gajiPokok + (gajiPokok / 20);
+        }
+        else {
+            if (istri.equalsIgnoreCase("y")&& anak == 0){
+                this.gaji = gajiPokok + (gajiPokok / 10) + (gajiPokok / 10);
+                this.gaji = gaji + (gaji / 10);
+            }
+            else if(istri.equalsIgnoreCase("n") && anak > 0 && anak <=3){
+                this.gaji = gajiPokok + (gajiPokok / 10) + (gajiPokok / 10);
+                this.gaji = gaji + (anak * (gaji * 3 / 20));
+            }
+            else if(istri.equalsIgnoreCase("y") && anak > 0 && anak <=3){
+                this.gaji = gajiPokok + (gajiPokok / 10) + (gajiPokok /10);
+                this.gaji = gaji + (gaji / 10) + (anak * (gaji * 3 / 20));
+            }
+            else if(istri.equalsIgnoreCase("y") && anak > 3) {
+                this.gaji = gajiPokok + (gajiPokok / 10) + (gajiPokok /10);
+                this.gaji = gaji + (gaji / 10) + (3 * (gaji * 3 / 20));
+            }
+            else {
+                this.gaji = gajiPokok + (gajiPokok / 10) + (gajiPokok /10);
+                this.gaji = gaji + (3 * (gaji * 3 / 20));
+            }
+        }
+    }
+    
+    public void Tampikan(){
+        System.out.println("===============================");
+        System.out.println("Nama Pegawai : "+nama);
+        System.out.println("Jabatan      : "+jabatan);
+        System.out.println("ID           : "+idKerja);
+        System.out.println("Tahun Masuk  : "+thn);
+        System.out.println("Gaji         : Rp."+gaji+"/Bulan");
+        System.out.println("===============================");
+    }
 }
