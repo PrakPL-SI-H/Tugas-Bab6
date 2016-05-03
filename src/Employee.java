a5c4199c9cef2d25a5b61815f826461dd56dc503
Nofry <nofryarman@gmail.com>
2016-05-03 15:04:39 -0700

    pembuatan class employeee

--- src/Employee.java
+++ src/Employee.java
@@ -9,5 +9,20 @@
  * @author Nofry
  */
 public class Employee {
+    public String nama;
+    public String jabatan;
+    public String idKerja;
+    public String istri;
+    public int anak, thn;
+    public final int gajiPokok = 4000000;
+    
+    public Employee (String nama, String jabatan, String idKerja, String istri, int anak, int thn){
+        this.nama = nama;
+        this.jabatan = jabatan;
+        this.idKerja = idKerja;
+        this.istri = istri;
+        this.anak = anak;
+        this.thn = thn;
+    }
     
 }
