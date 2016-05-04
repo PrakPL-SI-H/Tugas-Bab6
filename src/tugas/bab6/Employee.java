36bf280da71f079a3afe5e233fc63b4914a5e2f8
wenigustirahayu <wenigr97@gmail.com>
2016-05-04 15:30:27 +0700

    penambahan construcktor Employee

--- src/tugas/bab6/Employee.java
+++ src/tugas/bab6/Employee.java
@@ -10,4 +10,13 @@ public class Employee { //superclassnya
     public int tahunMasuk;
     public int gaji;
 
+    public Employee(String nama, String idKerja, String jabatan, String punyaIstri, int punyaAnak, int tahunMasuk) { //konstruktor superclass Employee
+        this.nama = nama;
+        this.idKerja = idKerja;
+        this.jabatan = jabatan;
+        this.punyaIstri = punyaIstri;
+        this.punyaAnak = punyaAnak;
+        this.tahunMasuk = tahunMasuk;
+    }
+
 }
