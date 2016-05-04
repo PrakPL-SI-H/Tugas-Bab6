554d64e25125186b62e4464c77ffd416edd44980
FelixYosa <felix_yosa@ymail.com>
2016-05-04 22:04:35 +0700

    edit

--- src/tugas/bab6/Employee.java
+++ src/tugas/bab6/Employee.java
@@ -3,6 +3,10 @@ package tugasemployee;
 
 public class Employee {
 
+    private String nama, idKerja, jabatan, istri, anak;
+    int tahun_masuk, jumlah_anak, lama, gaji = 20000;
+    double bonus, tunjangan;
+
 
 
 }
