552f71bc5f68f3c3ae2d8c4f8f65bea48b823fb7
wenigustirahayu <wenigr97@gmail.com>
2016-05-04 15:25:31 +0700

    buat class Employee sebagai superclassnya dengan atribut modifier public

---
+++ src/tugas/bab6/Employee.java
@@ -0,0 +1,13 @@
+package mainemployee;
+
+public class Employee { //superclassnya
+
+    public String nama;
+    public String idKerja;
+    public String jabatan;
+    public String punyaIstri;
+    public int punyaAnak;
+    public int tahunMasuk;
+    public int gaji;
+
+}
