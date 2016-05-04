cd01139bc15a9a87e9ba45f44456ac0d9fa780bf
FajarAH <akbaransyah@gmail.com>
2016-05-04 12:51:54 +0700

    Membuat method untuk mengeset atribut class PegawaiTidakTetap

--- LaporanBab6/src/PegawaiTidakTetap.java
+++ LaporanBab6/src/PegawaiTidakTetap.java
@@ -5,4 +5,11 @@ public class PegawaiTidakTetap extends Employee {
     public PegawaiTidakTetap(String name, String idKerja, String jabatan, boolean istri, boolean anak, int hireYear,int jumlahAnak) {
         super(name, idKerja, jabatan, istri, anak, hireYear, jumlahAnak);
     }
+    public void setJamKerja(int jam){
+        jamKerja = jam;
+    }
+    public void setHariKerja(int hari){
+        hariKerja = hari;        
+    }
+    
 }
