2b8f1b39abbcca6b98df42045d5e2d687eb67f8a
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-04 20:34:01 +0700

    penambahan method hitung gaji dengan if else

--- src/tugas/bab6/PegawaiTetap.java
+++ src/tugas/bab6/PegawaiTetap.java
@@ -16,5 +16,35 @@ public class PegawaiTetap extends Employee{
     public PegawaiTetap(String nama, String jabatan, String idKerja, String istri, int anak, int thn) {
         super(nama, jabatan, idKerja, istri, anak, thn);
     }
-     
+      public void HitungGaji(){
+        lamaKerja = 2016 - thn;
+        if (lamaKerja <= 5){
+            this.gajiPT = gajiDasar;
+        }
+        else if (lamaKerja >= 6 && lamaKerja <=10){
+            this.gajiPT = gajiDasar + (gajiDasar / 20);
+        }
+        else {
+            if (istri.equalsIgnoreCase("y")&& anak == 0){
+                this.gajiPT = gajiDasar + (gajiDasar / 10) + (gajiDasar / 10);
+                this.gajiPT = gajiPT + (gajiPT / 10);
+            }
+            else if(istri.equalsIgnoreCase("n") && anak > 0 && anak <=3){
+                this.gajiPT = gajiDasar + (gajiDasar / 10) + (gajiDasar / 10);
+                this.gajiPT = gajiPT + (anak * (gajiPT * 3 / 20));
+            }
+            else if(istri.equalsIgnoreCase("y") && anak > 0 && anak <=3){
+                this.gajiPT = gajiDasar + (gajiDasar / 10) + (gajiDasar /10);
+                this.gajiPT = gajiPT + (gajiPT / 10) + (anak * (gajiPT * 3 / 20));
+            }
+            else if(istri.equalsIgnoreCase("y") && anak > 3) {
+                this.gajiPT = gajiDasar + (gajiDasar / 10) + (gajiDasar /10);
+                this.gajiPT = gajiPT + (gajiPT / 10) + (3 * (gajiPT * 3 / 20));
+            }
+            else {
+                this.gajiPT = gajiDasar + (gajiDasar / 10) + (gajiDasar/10);
+                this.gajiPT = gajiPT + (3 * (gajiPT * 3 / 20));
+            }
+        }
+    }
 }
