b4db108fd9ab5e44cfdbb291716f8f1c8fe4a3bb
istighfarinba <bafarcorp@gmail.com>
2016-05-04 19:17:27 +0700

    inisialisasi nilai variabel gaji untuk pegawai tidak tetap

--- src/PegawaiTidakTetap.java
+++ src/PegawaiTidakTetap.java
@@ -11,6 +11,8 @@ public class PegawaiTidakTetap extends Employee {
 
     public PegawaiTidakTetap(String nama, String jab, String id, String status, int anak, int thn) {
         super(nama, jab, id, status, anak, thn);
-        
+        gaji = 2000000;//gaji default pegawai tidak tetap=2000000
     }
-}
+
+    }
+
