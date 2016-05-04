7c538433f5fec00c1687a0c9cf32db773ad4b0b8
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-04 20:24:18 +0700

    pembuatan kelas Manajer, sebagai extends class employee dan deklarasi super

---
+++ src/tugas/bab6/Manajer.java
@@ -0,0 +1,14 @@
+
+package laporan;
+
+public class Manajer extends Employee {
+    public int gajiM;
+    public int lamaKerjaM;
+ 
+
+
+    public Manajer(String nama, String jabatan, String idKerja, String istri, int anak, int thn) {
+        super(nama, jabatan, idKerja, istri, anak, thn);
+    }
+    
+}
