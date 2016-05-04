a3b57dfbdcb85165284ca8b135f08875db2948ff
istighfarinba <bafarcorp@gmail.com>
2016-05-04 19:32:08 +0700

    buat MAIN CLASS gan

---
+++ src/MainDataPerusahaan.java
@@ -0,0 +1,16 @@
+package laporaninherit;
+
+/**
+ *
+ * @author Istighfarin Bahtiar Amry
+ * @NIM 155150401111129
+ * @KElas SI-H
+ */
+import java.util.Scanner;
+
+public class MainDataPerusahaan {
+
+
+    public static void main(String[] args) {
+    }
+}
