2f1b980d3d4821ef753c8a37978835b0b56b6dd4
FelixYosa <felix_yosa@ymail.com>
2016-05-04 22:21:34 +0700

    edit

--- src/tugas/bab6/PegawaiTidakTetap.java
+++ src/tugas/bab6/PegawaiTidakTetap.java
@@ -10,4 +10,20 @@ public class PegawaiTidakTetap extends Employee {
         super(a, b, c, d, e, f, g);
     }
 
+    public void setJamKerja(int j) {
+        jam = j;
+    }
+
+    public double lembur() {
+        if (jam > 10) {
+            lembur = (jam - 10) * 10000;
+        } else {
+            lembur = 0;
+        }
+        return lembur;
+    }
+
+    public double total() {
+        return super.getgaji() + lembur;
+    }
 }
