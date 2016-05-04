58858a60b764fea03c8ce3aa8ec64258f235c40a
wenigustirahayu <wenigr97@gmail.com>
2016-05-04 15:58:30 +0700

    penambahan method void setBonus

--- src/tugas/bab6/pegawaiTetap.java
+++ src/tugas/bab6/pegawaiTetap.java
@@ -9,4 +9,25 @@ public class pegawaiTetap extends Employee {
         gaji = 2000000;
     }
 
+    public void setBonus(int tahunMasuk, String istri, int anak) {//method setter atau encapsulasi
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int tahun = currentYear - super.getTahunMasuk();
+        if (tahun < 5) {
+            gaji += 0;
+        } else if (tahun > 5 && tahun < 11) {
+            gaji += (gaji * (0.05) * tahun);
+        } else {
+            gaji += (gaji * (0.1 * tahun)) + (gaji * (0.1 * tahun));
+        }
+        if (istri.equalsIgnoreCase("ya") && anak == 0) {
+            gaji += (gaji * (0.1 * tahun));
+        } else if (istri.equalsIgnoreCase("ya") && anak < 4) {
+            gaji += (gaji * (0.1 * tahun)) + (gaji * (0.15 * tahun) * anak);
+        } else if (istri.equalsIgnoreCase("ya") && anak > 3) {
+            gaji += ((gaji * (0.1 * tahun)) + (gaji * (0.15 * tahun) * 3));
+        } else {
+            gaji += 0;
+        }
+    }
+
 }
