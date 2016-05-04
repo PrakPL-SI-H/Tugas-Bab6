43ddefb91d62278a614b3ed232aa5de237e84b5c
istighfarinba <bafarcorp@gmail.com>
2016-05-04 19:12:34 +0700

    Membuat child class untuk PegawaiTidakTetap.java+membuat konstruktornya+mengambil konstruktor parent class(super)

---
+++ src/PegawaiTidakTetap.java
@@ -0,0 +1,16 @@
+
+package laporaninherit;
+
+/**
+ *
+ * @author Istighfarin Bahtiar Amry
+ * @NIM 155150401111129
+ * @KElas SI-H
+ */
+public class PegawaiTidakTetap extends Employee {
+
+    public PegawaiTidakTetap(String nama, String jab, String id, String status, int anak, int thn) {
+        super(nama, jab, id, status, anak, thn);
+        
+    }
+}
