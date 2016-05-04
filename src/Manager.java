563311e2c5421e895d564663a7e8f8fa7bdf6ff7
istighfarinba <bafarcorp@gmail.com>
2016-05-04 18:55:15 +0700

    Membuat child class(Manager.java) pembuatan konstruktor dan pemanggilan konstruktor parent dengan keyword super

---
+++ src/Manager.java
@@ -0,0 +1,17 @@
+
+package laporaninherit;
+/**
+ *
+ * @author Istighfarin Bahtiar Amry
+ * @NIM    155150401111129
+ * @KElas  SI-H
+ */
+public class Manager extends Employee {
+
+    public Manager(String nama, String jabatan, String id,String status, int anak, int thn) {
+        super(nama, jabatan, id, status, anak, thn);
+        
+    }
+}
+
+
