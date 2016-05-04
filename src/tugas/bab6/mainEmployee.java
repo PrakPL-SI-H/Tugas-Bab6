eb742d4e7b493da1b6e291bdc5e83e0fa1edf091
FelixYosa <felix_yosa@ymail.com>
2016-05-04 22:49:39 +0700

    buat main

---
+++ src/tugas/bab6/mainEmployee.java
@@ -0,0 +1,16 @@
+package tugasemployee;
+
+import java.util.Scanner;
+
+public class mainEmployee {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int tahun_masuk = 0, jumlah_anak = 0, pilih;
+        String nama = "", id = "", jabatan = "", istri = "", anak = "";
+
+        do {
+
+        } while (pilih != 0);
+    }
+}
