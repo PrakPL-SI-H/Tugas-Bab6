cb096a26dfc22f15b8918b38e7de60a4ac9c6db2
FarisGenis <faaris513@gmail.com>
2016-05-04 21:13:46 +0700

    6

--- src/Bab6/PegawaiTakTetap.java
+++ src/Bab6/PegawaiTakTetap.java
@@ -1,6 +1,21 @@
-
 package Bab6;
 
-public class PegawaiTakTetap {
-    
+public class PegawaiTakTetap extends Employee {
+
+    public int Gaji;
+    public int Jam;
+
+    public PegawaiTakTetap(String Nama, String Jabatan, String Id, String Istri, int Anak, int Tahun, int Jam) {
+        super(Nama, Jabatan, Id, Istri, Anak, Tahun);
+        this.Jam = Jam;
+    }
+
+    public void GajiKotor() {
+        Gaji = GajiPokokPegTakTetap;
+        if (Jam > 10) {
+            Gaji += ((10000 * (Jam - 10)) * 30);
+        } else {
+            Gaji = GajiPokokPegTakTetap;
+        }
+    }
 }
