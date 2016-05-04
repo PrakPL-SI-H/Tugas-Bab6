b6f8ff1d45fb1e4060d045637e2681bed2c2a09e
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-04 20:43:44 +0700

    penambahan if-else pada main class

--- src/tugas/bab6/MainPegawai.java
+++ src/tugas/bab6/MainPegawai.java
@@ -32,6 +32,37 @@ public class MainPegawai {
          System.out.print("Apa jabatan anda sekarang di PT ini?(Manajer/Pegawai Tetap/Pegawai Tidak Tetap)         : ");
          jabatan = in.next();
          
+           if(jabatan.equalsIgnoreCase("Pegawai Tetap")){
+             System.out.print("Apakah anda sudah menikah/memiliki istri?(y/n)                                        : ");
+             istri = in.next();
+             System.out.print("Berapakah Jumlah Anak Anda?                                                           : ");
+             anak = in.nextInt();
+
+            PegawaiTetap TTP = new PegawaiTetap(nama,"Pegawai Tetap",idKerja,istri,anak,thn);
+             TTP.HitungGaji();
+             TTP.Tampikan();
+         }
+         else if(jabatan.equalsIgnoreCase("Manajer")){
+             System.out.print("Apakah anda sudah menikah/memiliki istri?(y/n)                                        : ");
+             istri = in.next();
+             System.out.print("Berapakah Jumlah Anak Anda?                                                           : ");
+             anak = in.nextInt();
+             Manajer BOSS = new Manajer(nama,jabatan,idKerja,istri,anak,thn);
+             BOSS.HitungGaji();
+             BOSS.Tampikan();
+         }
+
+        else if(jabatan.equalsIgnoreCase("Pegawai Tidak Tetap")){
+             System.out.print("masukkanlah Jumlah Jam kerja anda   : ");
+             jam = in.nextInt();
+
+            PegawaiNoTetap PTT = new PegawaiNoTetap(nama,"Pegawai Tidak Tetap",idKerja,"n",0,thn, jam);
+             PTT.HitungGaji();
+             PTT.Tampilkan();
+         }
+         else{
+             System.out.println("-Masukan Anda Tidak Ditemukan,Coba Ulang sesaat lagi-");
+         }
 System.out.print("Melanjutkan Program ini? (Y/N) ");
         answer = in.next();
     }while(answer.equalsIgnoreCase("Y"));
