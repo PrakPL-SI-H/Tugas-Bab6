f7b084fb8f252512c17c5c86533bf54c78044f5b
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-05-04 22:03:30 +0700

    add subclass PegawaiTetap

---
+++ src/tugas/bab6/PegawaiTetap.java
@@ -0,0 +1,7 @@
+package prakproglan.bab6;
+public class PegawaiTetap extends Employee{
+    
+    public PegawaiTetap(String nama, int id, String jabatan, String istri, int anak, int tahun){
+        super(nama, id, jabatan, istri, anak, tahun);       
+    }
+}
