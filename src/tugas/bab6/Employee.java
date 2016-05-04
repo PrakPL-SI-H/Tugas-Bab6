5e56d263d05b830c2e2a8f0e0a703263d98ccba3
FelixYosa <felix_yosa@ymail.com>
2016-05-04 22:10:28 +0700

    edit

--- src/tugas/bab6/Employee.java
+++ src/tugas/bab6/Employee.java
@@ -7,6 +7,20 @@ public class Employee {
     int tahun_masuk, jumlah_anak, lama, gaji = 20000;
     double bonus, tunjangan;
 
+    public Employee(String a, String b, String c, String d, String e, int f, int g) {
+        nama = a;
+        idKerja = b;
+        jabatan = c;
+        istri = d;
+        anak = e;
+        if (g > 3) {
+            jumlah_anak = 3;
+        } else {
+            jumlah_anak = g;
+        }
+        tahun_masuk = f;
+        lama = 2016 - tahun_masuk;
+    }
 
 
 }
