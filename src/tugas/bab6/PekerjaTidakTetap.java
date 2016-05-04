a0c6bcfd96e122c2772e6a876b48d6bb9e8b4586
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-04 22:12:32 +0700

    edit final method class

--- src/tugas/bab6/PekerjaTidakTetap.java
+++ src/tugas/bab6/PekerjaTidakTetap.java
@@ -1,37 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Inheritance;
-
-/**
- *
- * @author Prasetyo
- */
 public class PekerjaTidakTetap extends Employee {
-    private double bonus;
-    private double tunjangan;
-    private String name;
-    private int Idkerja;
-    private String Jabatan;
-    private String Status;
-    private int anak;
-    private int Tahun;
+    private int lembur;
+    public int nilai;
+    private int total;
+    private int gaji = 3000000;
     public PekerjaTidakTetap(String name,int Idkerja,String Jabatan,String Status,int anak,int Tahun){
         super (name, Idkerja, Jabatan, Status, anak, Tahun);//harus ada paling atas
     }
-    @Override
-    public String Getnama(){
-    return name;
+    public void lembur(int lembur){
+        if (10 <= lembur){
+            System.out.println("jika lebih dari 10 jam mendapat lembur");
+            nilai = lembur - 10;
+            total = 10000 * nilai;
+            System.out.println("Lembur      : "+total);
+        }else{
+            System.out.println("jika kurang dari 10 jam tidak mendapat lembur");
+            System.out.println("Lembur       : "+total);
+        }
     }
-    @Override
-    public String GetJabatan(){
-    return Jabatan;
-    }
-    @Override
-    public int GetIdKerja(){
-    return Idkerja;
-    }
-    
+    public void TampilPtt(){
+        System.out.println("Gaji        : "+ gaji );     
+    }  
 }
