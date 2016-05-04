18a08ce1190a25db74c778da50015038b28553c6
FelixYosa <felix_yosa@ymail.com>
2016-05-04 22:15:47 +0700

    edit

--- src/tugas/bab6/Employee.java
+++ src/tugas/bab6/Employee.java
@@ -4,7 +4,7 @@ package tugasemployee;
 public class Employee {
 
     private String nama, idKerja, jabatan, istri, anak;
-    int tahun_masuk, jumlah_anak, lama, gaji = 20000;
+    int tahunmasuk, jumlah_anak, tahunkerja, gaji = 20000;
     double bonus, tunjangan;
 
     public Employee(String a, String b, String c, String d, String e, int f, int g) {
@@ -18,8 +18,8 @@ public class Employee {
         } else {
             jumlah_anak = g;
         }
-        tahun_masuk = f;
-        lama = 2016 - tahun_masuk;
+        tahunmasuk = f;
+        tahunkerja = 2016 - tahunmasuk;
     }
 
     public void hitung(int tahun_masuk) {
@@ -27,12 +27,13 @@ public class Employee {
             bonus = 0;
             tunjangan = 0;
         } else if (2015 - tahun_masuk <= 6 && 2015 - tahun_masuk <= 10) {
-            bonus = 0.05 * gaji * lama;
+            bonus = 0.05 * gaji * tahunkerja;
             tunjangan = 0;
         } else if (2015 - tahun_masuk > 10) {
-            bonus = 0.1 * gaji * lama;
-            tunjangan = 0.1 * gaji * lama;
+            bonus = 0.1 * gaji * tahunkerja;
+            tunjangan = 0.1 * gaji * tahunkerja;
         }
+
         if (istri.equalsIgnoreCase("y") && anak.equalsIgnoreCase("y")) {
             tunjangan += (0.1 * (gaji + tunjangan)) + ((0.15 * jumlah_anak) * (tunjangan + gaji));
         } else if (istri.equalsIgnoreCase("y") && anak.equalsIgnoreCase("n")) {
@@ -44,5 +45,16 @@ public class Employee {
         }
     }
 
+    public int getgaji() {
+        return gaji;
+    }
+
+    public double getTunjangan() {
+        return tunjangan;
+    }
+
+    public double gajiTotal() {
+        return gaji + tunjangan + bonus;
+    }
 
 }
