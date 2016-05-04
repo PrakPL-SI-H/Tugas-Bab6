3f46dd7b293c8f4530b4924ba8724fd94c0c5e0a
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-04 20:11:48 +0700

    pembuatan class Employee sebagai superclass dengan variabel jabatan,ID,dll

---
+++ src/tugas/bab6/Employee.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laporan;
+
+/**
+ *
+ * @author TEGAR
+ */
+public class Employee {
+  
+      public String nama;
+     public String jabatan;
+     public String ID;
+     public String istri;
+     public int anak, thn;
+     public final int gajiDasar = 6000000;
+     
+   
+    
+    }   
+ 
