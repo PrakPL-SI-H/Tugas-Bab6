1faeaafd317954359e1dc7c9e0bee7b8b17a7d65
istighfarinba <bafarcorp@gmail.com>
2016-05-04 19:28:51 +0700

    add method setBonus+parameter+logika perhitungan untuk tahun dan anak

--- src/PegawaiTetap.java
+++ src/PegawaiTetap.java
@@ -12,7 +12,18 @@ public class PegawaiTetap extends Employee {
         super(nama, jab, id, status, anak, thn);
         gaji = 4000000; //gaji default PegawaiTetap=4000000
     }
-
+//penentu jumlah bonus berupa tahun,status,anak
+    public void setBonus(int thn, String status, int anak) {
+        if(thn<5){
+            gaji=4000000;
+        }
+        else if (thn >=6 && thn <= 10) {
+            gaji += (gaji * (0.05 / 100));
+        } else {
+            gaji += (gaji * (0.1 / 100)) + (gaji * (0.1 / 100));
+        }
+        
+    }
 }
 
 
