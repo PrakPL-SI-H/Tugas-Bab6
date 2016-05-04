ff817eff4465ae50e3242245dd8e110a855141ee
FarisGenis <faaris513@gmail.com>
2016-05-04 20:36:50 +0700

    4

--- src/Bab6/Manajer.java
+++ src/Bab6/Manajer.java
@@ -1,6 +1,36 @@
-
 package Bab6;
 
-public class Manajer {
-    
+public class Manajer extends Employee {
+
+    public int Gaji;
+    public int LamaKerja;
+
+    public Manajer(String Nama, String Jabatan, String Id, String Istri, int Anak, int Tahun) {
+        super(Nama, Jabatan, Id, Istri, Anak, Tahun);
+    }
+
+    public void Tunjangan() {
+        LamaKerja = 2016 - Tahun;
+        if (LamaKerja < 5) {
+            Gaji = 5000000;
+        } else if (LamaKerja >= 6 && LamaKerja <= 10) {
+            Gaji += (Gaji * (0.05 / 100));
+        } else {
+            Gaji += (Gaji * (0.1 / 100)) + (Gaji * (0.1 / 100));
+        }
+        if ("Y".equalsIgnoreCase(Istri) && Gaji == 0) {
+            Gaji += (Gaji * (0.1 / 100)) + (Gaji * (0.1 / 100));
+        } else if ("Y".equalsIgnoreCase(Istri) && Anak < 4) {
+            Gaji += (Gaji * (0.1 / 100)) + ((Gaji * (0.15 / 100)) * Anak) + (Gaji * (0.1
+                    / 100));
+        } else if ("Y".equalsIgnoreCase(Istri) && Anak > 3) {
+            Gaji += (Gaji * (0.1 / 100)) + ((Gaji * (0.15 / 100)) * 3) + (Gaji * (0.1 / 100));
+        } else if ("N".equalsIgnoreCase(Istri) && Anak < 4) {
+            Gaji += ((Gaji * (0.15 / 100)) * Anak) + (Gaji * (0.1 / 100));
+        } else if ("N".equalsIgnoreCase(Istri) && Anak > 3) {
+            Gaji += ((Gaji * (0.15 / 100)) * 3) + (Gaji * (0.1 / 100));
+        } else {
+            Gaji += (Gaji * (0.1 / 100));
+        }
+    }
 }
--- src/Bab6/PegawaiTetap.java
+++ src/Bab6/PegawaiTetap.java
@@ -1,6 +1,10 @@
 
 package Bab6;
 
-public class PegawaiTetap {
+public class PegawaiTetap extends Employee {
+    
+    public PegawaiTetap(String Nama, String Jabatan, String Id, String Istri, int Anak, int Tahun) {
+        super(Nama, Jabatan, Id, Istri, Anak, Tahun);
+    }
     
 }
