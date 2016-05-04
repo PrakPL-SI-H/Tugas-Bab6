0a6157e1971b23cc4fb6b19870fe53f6f59fd215
istighfarinba <bafarcorp@gmail.com>
2016-05-04 18:56:35 +0700

    Inisialisasi nilai variabel gaji pada class Manager

--- src/Manager.java
+++ src/Manager.java
@@ -10,7 +10,7 @@ public class Manager extends Employee {
 
     public Manager(String nama, String jabatan, String id,String status, int anak, int thn) {
         super(nama, jabatan, id, status, anak, thn);
-        
+        gaji = 5000000; //gaji default manager =5000000
     }
 }
 
