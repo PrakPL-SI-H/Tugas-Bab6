51cbdbee638555ecb39d4d77f0442b9adcd82c6a
salsasyafta <salsasyafta@gmail.com>
2016-05-04 12:24:55 +0700

    kelas main yuhuu

---
+++ src/MainEmployee.java
@@ -0,0 +1,39 @@
+package PrakSIH;
+public class MainEmployee {
+    public static void main(String[] args) {
+        Manager m1 = new Manager ();
+        m1.cetakManager();
+        
+        PegawaiTetap pt1 = new PegawaiTetap();
+        pt1.cetakPegawaiTetap();
+        PegawaiTetap pt2 = new PegawaiTetap();
+        pt2.cetakPegawaiTetap();
+        PegawaiTetap pt3 = new PegawaiTetap();
+        pt3.cetakPegawaiTetap();
+        PegawaiTetap pt4 = new PegawaiTetap();
+        pt4.cetakPegawaiTetap();
+        PegawaiTetap pt5 = new PegawaiTetap();
+        pt5.cetakPegawaiTetap();
+        PegawaiTetap pt6 = new PegawaiTetap();
+        pt6.cetakPegawaiTetap();
+        PegawaiTetap pt7 = new PegawaiTetap();
+        pt7.cetakPegawaiTetap();
+        PegawaiTetap pt8 = new PegawaiTetap();
+        pt8.cetakPegawaiTetap();
+        PegawaiTetap pt9 = new PegawaiTetap();
+        pt9.cetakPegawaiTetap();
+        PegawaiTetap pt10 = new PegawaiTetap();
+        pt10.cetakPegawaiTetap();
+        
+        PegawaiTidakTetap nonpns1 = new PegawaiTidakTetap();
+        nonpns1.cetakTidakTetap();
+        PegawaiTidakTetap nonpns2 = new PegawaiTidakTetap();
+        nonpns2.cetakTidakTetap();
+        PegawaiTidakTetap nonpns3 = new PegawaiTidakTetap();
+        nonpns3.cetakTidakTetap();
+        PegawaiTidakTetap nonpns4 = new PegawaiTidakTetap();
+        nonpns4.cetakTidakTetap();
+        PegawaiTidakTetap nonpns5 = new PegawaiTidakTetap();
+        nonpns5.cetakTidakTetap();
+    }
+}
