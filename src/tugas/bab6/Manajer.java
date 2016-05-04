9ac99c297621ac013c967c77584c95b8997aed7c
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-04 20:27:16 +0700

    tambah method hitunggaji

--- src/tugas/bab6/Manajer.java
+++ src/tugas/bab6/Manajer.java
@@ -9,6 +9,15 @@ public class Manajer extends Employee {
 
     public Manajer(String nama, String jabatan, String idKerja, String istri, int anak, int thn) {
         super(nama, jabatan, idKerja, istri, anak, thn);
+        
     }
-    
+    public void HitungGaji(){
+        lamaKerjaM = 2016 - thn;
+        if (lamaKerjaM <= 5){
+            this.gajiM = gajiDasar;
+        }
+        else {
+            this.gajiM = gajiDasar + (gajiDasar / 20);
+        }
+}
 }
