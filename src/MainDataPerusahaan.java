fff0fb42c66e39e87b1ebdf2b1c138f6afe1dc97
istighfarinba <bafarcorp@gmail.com>
2016-05-04 19:42:00 +0700

    membuat method non-static showTime agar lebih sederhana

--- src/MainDataPerusahaan.java
+++ src/MainDataPerusahaan.java
@@ -10,13 +10,25 @@ import java.util.Scanner;
 
 public class MainDataPerusahaan {
 
-    public String daftar = "\n| ID Kerja\t| Nama Pegawai\t\t|Jabatan\t\t| Gaji Pokok\t\t|Gaji Total     |\n"
-            + "|=======================================================================================================|\n";
+    public String daftar = "\n| ID Kerja\t| Nama Pegawai\t\t"+ "|Jabatan\t\t"+ "| Gaji Pokok\t\t|Gaji Total     |\n"+ "|=======================================================================================================|\n";
     int gman = 5000000, pegt = 4000000, pegtt = 2000000;
 
     public static void main(String[] args) {
         MainDataPerusahaan go = new MainDataPerusahaan();
+        
 
     }
 
+    public void showTime() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("\t\t\t=================PT.BafarCorp Hi-Tech================");
+        System.out.println("\t\t\t    ===========GAJI PEGAWAI PT.BafarCorp===========");
+        System.out.println("");
+        System.out.print("Jumlah Data Yang Ingin Dimasukan : ");
+        int ndata = in.nextInt();
+        System.out.println();
+        
+        
+
+    }
 }
