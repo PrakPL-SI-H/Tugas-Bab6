ef74a17ea97db7818bc9c2c76b9eadeeec955c30
FajarAH <akbaransyah@gmail.com>
2016-05-04 13:09:03 +0700

    Membuat inputan untuk variabel name dan idKerja

--- LaporanBab6/src/MainEmployee.java
+++ LaporanBab6/src/MainEmployee.java
@@ -1,13 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author asus
- */
+import java.util.Scanner;
+
+
 public class MainEmployee {
-    
+    public static void main(String[] args) {
+      Scanner in = new Scanner (System.in);
+      String name, idKerja;
+      
+       System.out.println("=====================================================");
+            System.out.printf("%35s \n","PENDATAAN KARYAWAN");
+            System.out.println("=====================================================\n");
+            System.out.print("Masukkan Nama\t\t: ");
+            name = in.nextLine();
+            System.out.print("Masukkan ID Kerja\t: ");
+            idKerja = in.nextLine();
+    }  
 }
