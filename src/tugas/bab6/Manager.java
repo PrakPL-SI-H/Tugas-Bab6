2f0cc9813d6f410a3a32f68b5f7baf937534372f
FelixYosa <felix_yosa@ymail.com>
2016-05-04 22:17:32 +0700

    buat class manager

---
+++ src/tugas/bab6/Manager.java
@@ -0,0 +1,11 @@
+
+package tugasemployee;
+
+public class Manager extends Employee {
+
+    public Manager(String a, String b, String c, String d, String e, int f, int g) {
+        super(a, b, c, d, e, f, g);
+    }
+
+
+}
