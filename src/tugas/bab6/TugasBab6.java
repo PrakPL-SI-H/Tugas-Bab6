60ea612af843af6cc5304dcecc0d3b0dc1c390c7
Nofry Arman <Nofry Arman>
2016-05-04 08:17:50 -0700

    Penyelesaian tugas

--- src/tugas/bab6/TugasBab6.java
+++ src/tugas/bab6/TugasBab6.java
@@ -1,21 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tugas.bab5;
+package tugas.bab6;
+import java.util.Scanner;
+public class TugasBab6 {
+    private static Object YesNo;
 
-/**
- *
- * @author irwin deriyan
- */
-public class TugasBab5 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        Scanner in =  new Scanner(System.in); 
+        String YesNo;
+        int Pilihan;
+        do{
+        System.out.println("SISTEM INFORMASI PEGAWAI PT.EQUINOX");
+        System.out.println("=========MENU INPUT DATA===========");
+        System.out.println("1. List Jabatan                    ");
+        System.out.println("2. Inputan Data Diri & Output      ");
+        System.out.println("3. Exit                            ");
+        System.out.print("Maskkan Pilihan Menu:"                );
+        Pilihan = in.nextInt();
+        switch(Pilihan){
+            case 1: 
+                System.out.println("List Jabatan Dalam Perusahaan           ");
+                System.out.println("- Manager                              ");
+                System.out.println("- Regular tetap  (RegularT)            ");
+                System.out.println("- Regular Tidak Tetap (RegularTT)      ");
+                break;
+            case 2: 
+                System.out.println("Input Output Data Pegawai");
+                System.out.print("Masukkan Nama        : ");
+                String nama = in.next();
+                System.out.print("Masukkan ID          : ");
+                String IdKerja = in.next();
+                System.out.print("Tahun Masuk          : ");
+                int Tahun = in.nextInt();
+                System.out.print("Masukkan Jabatan     : ");
+                String Jabatan = in.next();
+                
+                if(Jabatan.equalsIgnoreCase("Manager")){
+                System.out.print("Status Hubungan (Menikah/Belum ) : ");
+                String status = in.next();
+                System.out.print("Jumlah Anak                             : ");
+                int Jmlanak = in.nextInt();
+                Manager Manager1 = new Manager(nama,"Manager",IdKerja,status,Jmlanak,Tahun);
+                Manager1.HitungGaji();
+                Manager1.Show();
+                   }
+            else   if(Jabatan.equalsIgnoreCase("RegularT")){
+                System.out.print("Status Hubungan (Menikah/Belum Menikah) : ");
+                String status = in.next();
+                System.out.print("Jumlah Anak           : ");
+                int Jmlanak = in.nextInt();
+                Ptetap RegularT = new Ptetap(nama,"Reguler Tetap",IdKerja,status,Jmlanak,Tahun);
+                RegularT.HitungGaji();
+                RegularT.Show();
+        }   
+            else if(Jabatan.equalsIgnoreCase("RegularTT")){
+                System.out.print("Jumlah Jam Bekerja    : ");
+                int jam = in.nextInt();
+                Ptidaktetap RegularTT = new Ptidaktetap(nama,"Reguler Tidak Tetap",IdKerja,"Belum Menikah",0,Tahun, jam);
+                RegularTT.HitungGaji();
+                RegularTT.Show();
+            } 
+             break;
+            default: System.out.println("Program Error!");
+        }
+        }while( Pilihan < 2);
+        System.out.println("Trasaksi Telah Berakhir");
+        }
 }
+    
+
+
+
