6ea7079a9e391b4f22dca1984673fd8b1ba99574
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-04 20:28:24 +0700

    edit method hitung gaji

--- src/tugas/bab6/Manajer.java
+++ src/tugas/bab6/Manajer.java
@@ -16,8 +16,31 @@ public class Manajer extends Employee {
         if (lamaKerjaM <= 5){
             this.gajiM = gajiDasar;
         }
-        else {
+        else if (lamaKerjaM >= 6 && lamaKerjaM <=10){
             this.gajiM = gajiDasar + (gajiDasar / 20);
         }
+        else {
+            if (istri.equalsIgnoreCase("y")&& anak == 0){
+                this.gajiM = gajiDasar + (gajiDasar / 10) + (gajiDasar / 10);
+                this.gajiM = gajiM + (gajiM / 10) + (gajiM / 10) ;
+            }
+            else if(istri.equalsIgnoreCase("n") && anak > 0 && anak <=3){
+                this.gajiM = gajiDasar + (gajiDasar / 10) + (gajiDasar / 10);
+                this.gajiM = gajiM + (gajiM / 10) +(anak * (gajiM * 3 / 20));
+            }
+            else if(istri.equalsIgnoreCase("y") && anak > 0 && anak <=3){
+                this.gajiM = gajiDasar + (gajiDasar / 10) + (gajiDasar /10);
+                this.gajiM = gajiM + (gajiM / 10)+ (gajiM / 10)+(anak * (gajiM * 3 / 20));
+            }
+            else if(istri.equalsIgnoreCase("y") && anak > 3) {
+                this.gajiM = gajiDasar + (gajiDasar / 10) + (gajiDasar /10);
+               this.gajiM = gajiM + (gajiM / 10) + (gajiM / 10)+ (3 * (gajiM * 3 / 20));
+            }
+            else {
+                this.gajiM = gajiDasar + (gajiDasar / 10) + (gajiDasar /10);
+                this.gajiM = gajiM + (3 * (gajiM * 3 / 20))+ (gajiM / 10);
+            }
+        }
+    }
 }
-}
+
