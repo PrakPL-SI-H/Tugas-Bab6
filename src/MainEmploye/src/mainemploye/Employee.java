84795c5135064741a7c21fc01347c2fb85a65b46
Johan Fajar <johanrubik@gmail.com>
2016-05-04 19:45:47 +0700

    2

--- src/MainEmploye/src/mainemploye/Employee.java
+++ src/MainEmploye/src/mainemploye/Employee.java
@@ -1,36 +1,65 @@
 package mainemploye;
 
 public class Employee {
-  
-  private String nama;
-  private String id;
-  private String jabatan;
-  private boolean istri;
-  private boolean anak;
-  private int masuk;
-  
-  public void setNama(String nama){
-      this.nama = nama;
-  }
-  
-  public void setId(String id){
-      this.id = id;
-  }
-  
-  public void setJabatan(String jabatan){
-      this.jabatan = jabatan;
-  }
-  
-  public void setIstri(boolean istri){
-      this.istri = istri;
-  }
-  
-  public void setAnak(boolean anak){
-      this.anak = anak;
-  }
-  
-  public void setMasuk(int Masuk){
-      this.masuk = masuk;
-  }
-          
+
+    String nama;
+    String id;
+    String jabatan;
+    boolean istri;
+    boolean anak;
+    int masuk;
+    double bonus;
+    double tunjangan;
+    double gaji;
+
+//    public void setNama(String nama) {
+//        this.nama = nama;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public void setJabatan(String jabatan) {
+//        this.jabatan = jabatan;
+//    }
+//
+//    public void setIstri(boolean istri) {
+//        this.istri = istri;
+//    }
+//
+//    public void setAnak(boolean anak) {
+//        this.anak = anak;
+//    }
+//
+//    public void setMasuk(int Masuk) {
+//        this.masuk = masuk;
+//    }
+//    
+    public double setBonus(){
+        if  (masuk < 2011 && masuk >2005){
+            bonus = 0.05*gaji;
+            gaji = gaji + bonus;
+        }
+        else if (masuk < 2005){
+            bonus = 0.1*gaji;
+            gaji = gaji + bonus;
+        }
+        return gaji;
+    }
+    
+    public double setTunjangan(){
+        if (istri == true){
+            tunjangan = gaji*0.1;
+            gaji = gaji + tunjangan;
+        }
+        if (anak == true){
+            tunjangan = gaji*0.15;
+            gaji = gaji + tunjangan;
+        }
+        return gaji;
+    }
+    
+    
+    
 }
