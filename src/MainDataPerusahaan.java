90388c886ba5cfb4c5cbed3a30d8d89079082e3f
istighfarinba <bafarcorp@gmail.com>
2016-05-04 19:35:08 +0700

    deklarasi variabel daftar(untuk output)  menginstansi objek baru dari classnya sendiri(niatnya mau bikin method non-static)

--- src/MainDataPerusahaan.java
+++ src/MainDataPerusahaan.java
@@ -10,7 +10,13 @@ import java.util.Scanner;
 
 public class MainDataPerusahaan {
 
+    public String daftar = "\n| ID Kerja\t| Nama Pegawai\t\t|Jabatan\t\t| Gaji Pokok\t\t|Gaji Total     |\n"
+            + "|=======================================================================================================|\n";
+    int gman = 5000000, pegt = 4000000, pegtt = 2000000;
 
     public static void main(String[] args) {
+        MainDataPerusahaan go = new MainDataPerusahaan();
+
     }
+
 }
