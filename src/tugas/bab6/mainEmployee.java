b5cb8c5478a49c485cbf59fc1c0315b046a08676
wenigustirahayu <wenigr97@gmail.com>
2016-05-04 16:13:25 +0700

    buat mainEmployee

---
+++ src/tugas/bab6/mainEmployee.java
@@ -0,0 +1,9 @@
+package mainemployee;
+
+import java.io.*;
+
+public class mainEmployee {
+
+    static String data = "     DATA PEGAWAI 2016    \n" + "\n| ID Kerja\t| Nama Pegawai\t\t| Jabatan\t\t| Gaji\t\t|\n" + "|=================================================================================|\n";
+
+}
