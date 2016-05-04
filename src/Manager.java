0ba859d6f7a0fc3a23a3c3d71297dae8213b198d
istighfarinba <bafarcorp@gmail.com>
2016-05-04 18:59:27 +0700

    logika perhitungan gaji berdasarkan variabel thn(lama bekerja)

--- src/Manager.java
+++ src/Manager.java
@@ -14,7 +14,14 @@ public class Manager extends Employee {
     }
 //penentu jumlah bonus merupakan tahun,status,dan anak
     public void setBonus(int thn, String status, int anak) {
-        
+        if(thn<5){
+            gaji=5000000;
+        }
+        else if (thn >=6 && thn<= 10) {
+            gaji += (gaji * (0.05 / 100));
+        } else {
+            gaji += (gaji * (0.1 / 100)) + (gaji * (0.1 / 100));
+        }
     }
 }
 
