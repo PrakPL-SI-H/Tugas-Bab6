6e6cceed09eb50972d45532ef12f1ff903f36e11
FelixYosa <felix_yosa@ymail.com>
2016-05-04 22:18:07 +0700

    buat class pegawaitidaktetap

---
+++ src/tugas/bab6/PegawaiTidakTetap.java
@@ -0,0 +1,13 @@
+
+package tugasemployee;
+
+public class PegawaiTidakTetap extends Employee {
+
+    private int jam;
+    private double lembur;
+
+    public PegawaiTidakTetap(String a, String b, String c, String d, String e, int f, int g) {
+        super(a, b, c, d, e, f, g);
+    }
+
+}
